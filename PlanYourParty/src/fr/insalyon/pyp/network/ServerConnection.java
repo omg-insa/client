8fdcfc73d94650eff760b0b0b7808e432568386b
pierre <p.monjallon@gmail.com>
2013-02-20 11:23:23 +0100

    get places

--- PlanYourParty/src/fr/insalyon/pyp/network/ServerConnection.java
+++ PlanYourParty/src/fr/insalyon/pyp/network/ServerConnection.java
@@ -11,34 +11,34 @@ public class ServerConnection {
 
 	private String requestUrl;
 
-	//base
+	// base
 	private static final String LOCAL_URL = "http://10.0.2.2:8080/api/";
 	private static final String REMOTE_URL = "http://planyourpty.appspot.com/api/";
 
 	static final boolean LOCAL = false;
 
-	//services
+	// services
 	public static final String LOGIN = "login/";
 	public static final String REGISTER = "register/";
 	public static final String GET_SECRET_QUESTION_FOR_RECOVERY = "get_secret_question_for_recovery/";
 	public static final String CHECK_SECRET_ANSWER = "check_secret_answer/";
 	public static final String UPDATE_PASSWORD = "update_password/";
 	public static final String UPDATE_QUESTION = "update_secret_question/";
-    public static final String GET_QUESTION_SECRET = "get_secret_question/";
+	public static final String GET_QUESTION_SECRET = "get_secret_question/";
+	public static final String GETEVT = "get_places/";
 
-	private ServerConnection()
-	{
-		if(LOCAL)
+	private ServerConnection() {
+		if (LOCAL)
 			setRequestUrl(LOCAL_URL);
 		else
 			setRequestUrl(REMOTE_URL);
 	}
 
-	public static ServerConnection GetServerConnection()
-	{
-		if(serverConnection == null)
+	public static ServerConnection GetServerConnection() {
+		if (serverConnection == null)
 			return new ServerConnection();
-		else return serverConnection;
+		else
+			return serverConnection;
 	}
 
 	public JSONObject connect(String service, List<NameValuePair> parameters) throws Exception {
@@ -47,12 +47,17 @@ public class ServerConnection {
 		setRequestUrl(getRequestUrl() + service);
 		try {
 		if(service.equals(LOGIN))
-			{
-				request = new HttpRequest(requestUrl, parameters);
-				returnObj = request.executePost();
-				AppTools.debug(returnObj.toString());
-			}
-	
+		{
+			request = new HttpRequest(requestUrl, parameters);
+			returnObj = request.executePost();
+			AppTools.debug(returnObj.toString());
+		}
+		if(service.equals(GETEVT))
+		{
+			request = new HttpRequest(requestUrl, parameters);
+			returnObj = request.executePost();
+			AppTools.debug(returnObj.toString());
+		}
 		if(service.equals(REGISTER))
 		{
 			request = new HttpRequest(requestUrl, parameters);
@@ -82,7 +87,7 @@ public class ServerConnection {
 			throw e;
 		}
 	}
-	
+
 	public String getRequestUrl() {
 		return requestUrl;
 	}
