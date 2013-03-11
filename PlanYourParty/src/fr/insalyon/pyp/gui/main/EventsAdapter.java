d63af99a4674452d8b15e985db2e16c091bf8b2b
Stefan CHITIC <chiticstefan@gmail.com>
2013-03-11 11:31:21 +0100

    swipe over lists

--- PlanYourParty/src/fr/insalyon/pyp/gui/main/EventsAdapter.java
+++ PlanYourParty/src/fr/insalyon/pyp/gui/main/EventsAdapter.java
@@ -11,7 +11,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import fr.insalyon.pyp.R;
 import fr.insalyon.pyp.gui.common.BaseActivity;
-import fr.insalyon.pyp.tools.AppTools;
 
 public class EventsAdapter extends BaseAdapter {
 
